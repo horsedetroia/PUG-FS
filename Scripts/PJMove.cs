5bb64e32d4511f28aa8196d1a895050a611f11d7
jayme <149827383+horsedetroia@users.noreply.github.com>
2026-09-01 09:02:02 -0300

    Update PJMove.cs
    PJ move script

--- Scripts/PJMove.cs
+++ Scripts/PJMove.cs
@@ -3,14 +3,19 @@ using UnityEngine;
 public class PJMove : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float velocidade = 5.0f;
+    public Rigidbody2D rb;
+    Vector2 forçaPulo;
+    bool podePular = true;
     void Start()
     {
-        
+        forçaPulo = new Vector2(0.0f, 5.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float mX = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocityX = mX * velocidade;
     }
 }
